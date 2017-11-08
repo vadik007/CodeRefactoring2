fd5935bd8c43bad126c0aae8b687dffba58e71a2
Vadim Bolshakov <vadim_bolshakov@epam.com>
2017-11-08 22:47:44 +0300

    init

---
+++ CodeRefactoring2/CodeRefactoring2.Vsix/ToolWindow1Control.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Composition;
+using Microsoft.VisualStudio.LanguageServices;
+
+namespace CodeRefactoring2.Vsix
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+
+
+    /// <summary>
+    /// Interaction logic for ToolWindow1Control.
+    /// </summary>
+    public partial class ToolWindow1Control : UserControl
+    {
+        [Import(nameof(VisualStudioWorkspace))]
+        public VisualStudioWorkspace MyWorkspace { get; set; }
+
+        public static string[] Items = { "1","2","3" };
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1Control"/> class.
+        /// </summary>
+        public ToolWindow1Control()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'. My workspace {1}", this.ToString(), MyWorkspace?.ToString() ?? "Null"),
+                "ToolWindow1");
+        }
+    }
+}
