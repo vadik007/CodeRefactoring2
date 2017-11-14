36da543b45b4223c6e9671db987a0a67ccfd450e
Vadim Bolshakov <vadim_bolshakov@epam.com>
2017-11-15 01:23:31 +0300

    Index structure

---
+++ CodeRefactoring2/CodeRefactoring2.Vsix/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRefactoring2.Vsix
+{
+    class HashedSourceFile
+    {
+        public int FileHash;
+
+        public SortedDictionary<int, SourceEntry> Entries { get; set; } = new SortedDictionary<int, SourceEntry>();
+        public void ProcessFile()
+        {
+            Entries
+        }
+    }
+
+    class SourceEntry
+    {
+        public int FileHash { get; set; }
+        public int Offset { get; set; }
+
+        public int PreviousHash { get; set; }
+
+        public int NextHash { get; set; }
+    }
+}
--- CodeRefactoring2/CodeRefactoring2/readme.md
+++ CodeRefactoring2/CodeRefactoring2/readme.md
@@ -1,4 +1,16 @@
-﻿1 log <=> src
+﻿# The idea YALC
+
+1 log <=> src
 2 (str, str, str) => Hash, Hash, Hash
 3 int[] <=> int[]
-4 Sorted look up map? 
+4 Sorted look up map?
+5.1 [sorted {hash, file hash, !symbol! position in file}] hash position in file - wont't work with regex "(.*)+?" (should be lazy) filtering(only position in file is known)
+5.2 [{-1, 34326, 4}, {-1, 34326, 5}]
+
+....."3 56 6567"
+................
+.."564 45 6"....
+
+=> 
+
+https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
